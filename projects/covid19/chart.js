b3377af4e211ac6ee2040a3610cd19af040d43c9
Swapnil <swapnil.bhoite@live.com>
2020-03-29 14:17:31 -0700

    Fix colors

--- projects/covid19/chart.js
+++ projects/covid19/chart.js
@@ -24,9 +24,6 @@ var chart_options = {
     legend: { position: 'top' },
     width: '100%',
     height: 300,
-    chartArea: {
-        width: '80%'
-    },
     vAxis: {
         viewWindow: {
             min: 0
@@ -36,13 +33,13 @@ var chart_options = {
 
 function drawCharts() {
     drawChart(document.getElementById('chart_div_1'), 'B5:C', 'INDIA', series_1)
-    drawChart(document.getElementById('chart_div_2'), 'B5:B,D5:D', 'INDIA', series_2)
-    drawChart(document.getElementById('chart_div_3'), 'B5:B,E5:E', 'INDIA', series_1)
+    drawChart(document.getElementById('chart_div_2'), 'B5:B,D5:D', 'INDIA', series_1)
+    drawChart(document.getElementById('chart_div_3'), 'B5:B,E5:E', 'INDIA', series_2)
     drawChart(document.getElementById('chart_div_4'), 'B5:B,F5:F', 'INDIA', series_2)
 
     drawChart(document.getElementById('chart_div_5'), 'B5:C', 'USA', series_1)
-    drawChart(document.getElementById('chart_div_6'), 'B5:B,D5:D', 'USA', series_2)
-    drawChart(document.getElementById('chart_div_7'), 'B5:B,E5:E', 'USA', series_1)
+    drawChart(document.getElementById('chart_div_6'), 'B5:B,D5:D', 'USA', series_1)
+    drawChart(document.getElementById('chart_div_7'), 'B5:B,E5:E', 'USA', series_2)
     drawChart(document.getElementById('chart_div_8'), 'B5:B,F5:F', 'USA', series_2)
 }
 
